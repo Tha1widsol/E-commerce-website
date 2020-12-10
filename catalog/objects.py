3cbd983fda7f99bee17911f1a29f60b4f506b50b
Kelvin <laikelvin8@gmail.com>
2020-12-10 11:06:50 +0000

    Added more TVs

--- catalog/objects.py
+++ catalog/objects.py
@@ -19,8 +19,11 @@ mycursor.execute("CREATE TABLE IF NOT EXISTS Item(itemID int PRIMARY KEY AUTO_IN
 
 
 
-
-mycursor.execute("INSERT INTO Item(type,name,description,price,picfile) VALUES ('TV','Samsung Q355Q60R','Samsungs QLED is certified to deliver 100% colour volume.',749.99,'tv1.jpg')")
+mycursor.execute("""INSERT INTO Item(type,name,description,price,picfile) VALUES
+('TV','Samsung Q355Q60R','Samsungs QLED is certified to deliver 100% colour volume.',749.99,'tv1.jpg'),
+('TV','Hitachi 58 Inch Smart 4K UHD HDR LED Freeview TV','Bring the cinema to your living room with this 58 inch Hitachi Smart TV. Featuring 4K Ultra HD resolution, you can catch every little detail on your screen.',369.99,'tv2.webp'),
+('TV','PHILIPS Ambilight 65" Smart 4K Ultra HD','With an OLED panel, every single pixel is controlled individually, giving deeper blacks, more vibrant colours and better contrast for an image that will look and feel stunningly lifelike.','2399','tv3.jpg')
+""")
 
 
 
