a7c8f75acf4bb6d7202ece6d866df46760af8f79
Kelvin <laikelvin8@gmail.com>
2020-12-16 09:55:50 +0000

    Added vaccum cleaners page

--- catalog/second.py
+++ catalog/second.py
@@ -8,11 +8,18 @@ second = Blueprint("second",__name__,static_folder="static",template_folder="tem
 @second.route("/TVs")
 def tv_page():
     values= "TV"
-    mycursor.execute("""SELECT * FROM Item WHERE type=TV""")
+    mycursor.execute("""SELECT * FROM Item WHERE type=%s """,(values,))
     items=mycursor.fetchall()
     
-    
     return render_template("items.html",page_name="TV's",database=items,tvtab="active",foldername="tvs")
+
+@second.route("/Vaccumcleaners")
+def vaccum_cleaners_page():
+    values="Vaccumcleaners"
+    mycursor.execute("""SELECT * FROM Item WHERE type=%s """,(values,))
+    items=mycursor.fetchall()
+
+    return render_template("items.html",page_name="Vaccum cleaners",database=items,vaccumtab="active",foldername="vaccumcleaners")
 
 
 
