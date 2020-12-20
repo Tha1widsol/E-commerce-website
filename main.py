62178d2477021c92919a8a579115c0358b39b180
Kelvin <laikelvin8@gmail.com>
2020-12-20 10:17:47 +0000

    Changed main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask,render_template,redirect,url_for,request,session,flash
 import os 
 from catalog.second import *
 from base.Home import *
-
+from admin.register import *
 
 
 
@@ -10,6 +10,7 @@ app = Flask(__name__)
 app.register_blueprint(second,url_prefix="/") # We only go on second blueprint if we see "/admin"
 #It finds "/" then adds "admin" = "/admin" 
 app.register_blueprint(home,url_prefix="/")
+app.register_blueprint(register,url_prefix="/")
 
 if __name__ =="__main__":
     app.run(debug=True,use_reloader=False)
