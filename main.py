d86d5a3a215be0514d27dab9d07f4b88427e6c1c
Kelvin <laikelvin8@gmail.com>
2021-05-07 16:47:15 +0100

    Added changes to cpython

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app.register_blueprint(home,url_prefix="/")
 app.register_blueprint(accounts,url_prefix="/")
 
 if __name__ =="__main__":
-    app.run(debug=True,use_reloader=False)
+    app.run(debug=True)
 
 
         
