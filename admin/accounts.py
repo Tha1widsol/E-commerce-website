c2497c6852871621bbdcf2d5f6ca5df89c6ceafb
Kelvin Lai <57137749+Tha1widsol@users.noreply.github.com>
2021-09-20 13:53:18 +0100

    Update accounts.py

--- admin/accounts.py
+++ admin/accounts.py
@@ -61,7 +61,7 @@ def register_page():
                         hashed = generate_password_hash(password)
                         new_user = Users(email=email,password = hashed)
                         db.session.add(new_user)
-                        new_user = Users.query.filter_by(email = email).first()
+                        new_user = get_user()
                         new_basket = Basket(user_id=new_user.id)
                         new_wishlist = wishlist(user_id= new_user.id)
                         db.session.add(new_wishlist)
